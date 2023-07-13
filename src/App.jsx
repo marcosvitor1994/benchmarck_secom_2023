aabdcba7112b3b00dedb7f4aa2c63337c223a178
MarcosVitor7 <6938130-MarcosVitor7@users.noreply.replit.com>
2023-07-13 21:42:30 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export default function App() {
+
+  
+  return (
+    <div className="df flex-col vh-100">
+      
+
+      <section className="flex-grow-1 bg-black-80 fw4 white-80 tc pt24">
+        
+        
+
+      <iframe title="Campanhas Finalizadas 2023" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiNGFkMWUxOTItNGY0ZS00NjEwLTg5YmYtMjQzOTU0MzlhZTdjIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9" frameborder="0" allowFullScreen="true"></iframe>
+
+                  
+                
+      </section>
+    </div>
+  )
+}
