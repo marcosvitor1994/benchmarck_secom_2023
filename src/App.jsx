d7be066c9737f7a38fcede0a940575befe7ca474
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2023-07-14 11:44:54 -0300

    Update App.jsx
    Atualização de Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,7 @@ export default function App() {
       <section className="flex-grow-1 bg-black-80 fw4 white-80 tc pt24">
         
         
-
-      <iframe title="Campanhas Finalizadas 2023" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiNGFkMWUxOTItNGY0ZS00NjEwLTg5YmYtMjQzOTU0MzlhZTdjIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9" frameborder="0" allowFullScreen="true"></iframe>
-
+      <iframe title="Campanhas Finalizadas 2023" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiNGFkMWUxOTItNGY0ZS00NjEwLTg5YmYtMjQzOTU0MzlhZTdjIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection6c811c3f74c96771caf5" frameborder="0" allowFullScreen="true"></iframe>
                   
                 
       </section>
