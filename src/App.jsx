3a0d3a990ce52797176f9c6dd3d71fab6215ac45
Marcos Vitor <70297708+marcosvitor1994@users.noreply.github.com>
2024-02-02 19:18:12 -0300

    Update Site.jsx
    Redirecionamento para o site correto.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
 
       <section className="flex-grow-1 bg-black-80 fw4 white-80 tc pt24">
         
-      <iframe title="Campanhas Finalizadas 2023 - Redes Sociais" width="100%" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZDViNTkzOGQtM2Q1Mi00MWQ1LWI0ZTEtN2JhOTRhYzUzZDVhIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection6c811c3f74c96771caf5" frameborder="0" allowFullScreen="true"></iframe>                     
+      <link>https://secom-app.vercel.app/</link>
                 
       </section>
     </div>
